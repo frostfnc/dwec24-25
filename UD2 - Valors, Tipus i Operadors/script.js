59c1972ebbeb237856400f8c267323eafdc82a80
Sergi Camps <sergicampsgili@gmail.com>
2024-09-30 15:52:49 +0200

    finish ud2

--- UD2 - Valors, Tipus i Operadors/script.js
+++ UD2 - Valors, Tipus i Operadors/script.js
@@ -80,4 +80,31 @@
 
 // console.log(undefined && 4 < 6 ? 1 : 2);
 
-console.log(8 * undefined);
+// console.log(8 * undefined);
+
+// let b;
+// console.log(b);
+
+// CONVERSIÓ AUTOMÀTICA DE TIPUS
+// console.log(typeof ("5" + 1)); //string
+// console.log(typeof (1 + "5")); //string
+// console.log(5 - "1"); //number
+// console.log("cinco" * 2);
+// console.log(false == 0); // true
+// console.log(true == 1); // true
+
+// console.log(null == null);
+// console.log(null == undefined);
+// console.log(null == 0);
+
+// Cortocircuito de los operadores && y ||
+// console.log(null || "joan"); // imprime joan
+// console.log("pepe" || "joan"); // imprime pepe
+// console.log("" || "joan"); // imprime joan
+
+console.log(0 || 100);
+console.log(0 && 100);
+
+// ?? -> se elije el lado de la derecha si el izquierdo es null o undefined
+console.log(0 ?? 100); // 0
+console.log(null ?? 100); // 100
