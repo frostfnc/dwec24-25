784fd3c611567fedffdc0b966dae5bac77d4a422
tryhard <sergicampsgili@gmail.com>
2024-10-02 21:12:59 +0200

    UD3

--- UD3 - Estructura del programa JavaScript/Control de Flux/script.js
+++ UD3 - Estructura del programa JavaScript/Control de Flux/script.js
@@ -49,9 +49,180 @@
 
 // DO WHILE
 
-let nombre;
-do {
-	nombre = prompt("¿Quíen eres?");
-} while (!nombre);
+// let nombre;
+// do {
+// 	nombre = prompt("¿Quíen eres?");
+// } while (!nombre);
 
-console.log(nombre);
+// console.log("Nombre: " + nombre);
+
+// BUCLES FOR
+
+// let numero = 2;
+// console.log("Antes del bucle:", numero);
+// for (numero = 0; numero <= 12; numero++) {
+// 	console.log(numero);
+// }
+// console.log("Despues del bucle:", numero);
+
+// for (let i = 0; i <= 100; i += 2) {
+// 	console.log(i);
+// }
+
+// for (let j = 0; j <= 100; j++) {
+// 	if (j % 2 == 0) {
+// 		console.log(j);
+// 	}
+// }
+
+// for (let numero = 20; ; numero++) {
+// 	if (numero % 7 == 0) {
+// 		console.log(numero);
+// 	}
+// }
+
+//switch - case
+
+// let pregunta = prompt("¿Que tiempo hace?");
+// switch (pregunta) {
+// 	case "lluvioso":
+// 		console.log("Coge un paraguas");
+// 		break;
+// 	case "soleado":
+// 		console.log("Lleva ropa ligera");
+// 	case "nublado":
+// 		console.log("Puedes salir a la calle");
+// 		break;
+// 	default:
+// 		console.log("Tiempo desconocido");
+// }
+
+/*
+ Ejercicio 1
+ Escribe un bucle que realice siete llamadas a console.log para mostras la siguiente figura:
+ #
+ ##
+ ###
+ ####
+ #####
+ ######
+ #######
+*/
+
+// let cadena = "#";
+// while (cadena.length <= 7) {
+// 	console.log(cadena);
+// 	cadena += "#";
+// }
+
+/* Ejercicio 2
+	Escribe un programa que use console.log para imprimir
+	todos los números del 1 al 100, con excepciones:
+ 	- Para los números que sean divisibles por 3, imprime: 
+ 	"Jander".
+ 	- Para los números divisibles por 5 (y no por 3), imprime:
+ 	"Clander".
+
+	Extension Pack
+	Modifica tu programa para imprimir "JanderClander" para los números que sean divisible por 3 y por 5 
+	(y sigue imprimiendo "Jander o "Clander" para los números que son divisibles solo por uno de esos casos).
+	
+	*/
+
+// for (let i = 1; i <= 100; i++) {
+// 	console.log(i);
+// 	if (i % 3 == 0) {
+// 		console.log("Jander : 3");
+// 	}
+// 	if (i % 5 == 0 && !(i % 3 == 0)) {
+// 		console.log("Clander : 5");
+// 	}
+// }
+
+// for (let i = 1; i <= 100; i++) {
+// 	if (i % 3 == 0 && !(i % 5 == 0)) {
+// 		console.log("Jander");
+// 	}
+// 	if (i % 5 == 0 && !(i % 3 == 0)) {
+// 		console.log("Clander");
+// 	}
+// 	if (i % 5 == 0 && i % 3 == 0) {
+// 		console.log("JanderClander");
+// 	}
+// 	console.log(i);
+// }
+
+/*
+	Escribe un programa que cree una cadena que represente un tablero de 8 x 8, usando caracteres de salto de línea para separar las líneas.
+	En cada posición del tablero hay que carácter de espacio o un carácter
+	"#". Los caracteres deben formar un tablero de ajedrez:
+*/
+
+// let tabla = "";
+// let col = 4;
+// let row = 50;
+// for (let i = 0; i < col; i++) {
+// 	for (let j = 0; j < row; j++) {
+// 		if ((i + j) % 2 == 0) {
+// 			tabla += " ";
+// 		} else {
+// 			tabla += "#";
+// 		}
+// 	}
+// 	tabla += "\n";
+// }
+// console.log(tabla);
+
+let pregunta = prompt("¿Que ejercicio quieres hacer?");
+switch (pregunta) {
+	case "1":
+		let cadena = "#";
+		while (cadena.length <= 7) {
+			console.log(cadena);
+			cadena += "#";
+		}
+		break;
+	case "2":
+		for (let i = 1; i <= 100; i++) {
+			console.log(i);
+			if (i % 3 == 0) {
+				console.log("Jander : 3");
+			}
+			if (i % 5 == 0 && !(i % 3 == 0)) {
+				console.log("Clander : 5");
+			}
+		}
+		break;
+	case "2 Extension Pack":
+		for (let i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && !(i % 5 == 0)) {
+				console.log("Jander");
+			}
+			if (i % 5 == 0 && !(i % 3 == 0)) {
+				console.log("Clander");
+			}
+			if (i % 5 == 0 && i % 3 == 0) {
+				console.log("JanderClander");
+			}
+			console.log(i);
+		}
+		break;
+	case "3":
+		let tabla = "";
+		let col = prompt("Dime las columnas de la tabla: ");
+		let row = prompt("Dime las filas de la tabla: ");
+		for (let i = 0; i < col; i++) {
+			for (let j = 0; j < row; j++) {
+				if ((i + j) % 2 == 0) {
+					tabla += " ";
+				} else {
+					tabla += "#";
+				}
+			}
+			tabla += "\n";
+		}
+		console.log(tabla);
+		break;
+	default:
+		console.log("No se ha introducido correctamente el nombre de un ejercicio.");
+}
